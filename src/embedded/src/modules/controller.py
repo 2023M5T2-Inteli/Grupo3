386e0cd01529c8ccc9440c4d4e6fbc213fcd5ab3
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2023-03-09 17:53:16 -0300

    feat:Robot movement did for all members

--- src/embedded/src/modules/controller.py
+++ src/embedded/src/modules/controller.py
@@ -34,13 +34,18 @@ def turnOffElectromagnet():
     MagicBox.IO.set_pin_value(12, 0)
     Time.sleep(500)
 
+def shakeAfterTurnOff():
+    i = 0
+    while i < 4:
+        Dobot.move_to(-53, 220, 20, 0)
+        Dobot.move_to(-53, 220, 0, 0)
+        i += 1 
+
 
 def showPosition():
     position = Dobot.get_pose()
     positionStr = "X {0} Y:{1} Z:{2}".format(position[0], position[1], position[2])
     Display.print(positionStr)
-
-
 
 def iniFirstTray():
     Display.print("Scanneando primeira bandeja!")
@@ -51,41 +56,75 @@ def iniFirstTray():
 
 
 def scanFirstTray():
-    repeat = 1
-    MoveZ = -53
+    repeat = 0
+    MoveX = -53
     turnOnElectromagnet()
     while repeat != 10:
         if (repeat % 2 == 0):
-            moveX = -220
+            moveY = -220
         else:
-            moveX = -340     
-        Dobot.move_to(MoveZ,  moveX,  -67, 0)
+            moveY = -340     
+        Dobot.move_to(MoveX,  moveY,  -67, 0)
         Time.sleep(1500)
-        MoveZ = MoveZ +10
+        MoveX = MoveX +10
         repeat = repeat + 1
 
 def iniSecondTray():
     Display.print("Separando na segunda bandeja!")
-    Dobot.move_to(0,  100,  10, 0)
+    Dobot.move_to(248,  -4,  -48, 0)
     Time.sleep(1000)
     # Dobot.move_to(-53,  -220,  -67, 0)
     # Time.sleep(1000)
 
+def shakeSecondTray():
+    repeat2 = 1
+    MoveX = 248
+    while repeat2 != 10:
+        if (repeat2 % 2 == 0):
+            moveY = -60
+        else:
+            moveY = -4    
+        Dobot.move_to(MoveX,  moveY,  -48, 0)
+        Time.sleep(1500)
+        MoveX = MoveX -10
+        repeat2 = repeat2 + 1
+
+def thirdTray():
+    
+    Display.print("Separando na terceira bandeja!")
+    Dobot.move_to(-53, 220, 0, 0)
+    Dobot.move_to(-53, 220, -65, 0)
+    turnOffElectromagnet()
+    Dobot.move_to(-53, 300, -65, 0)
+    Dobot.move_to(-53, 220, -65, 0)
+    upArm()
+    
+    
 
 def upArm():
     [x,y,z,r] = Dobot.get_pose()[0:4]
     Dobot.move_to(x, y,  0, 0)
 
+def finish():
+    Dobot.set_home()
+    Dobot.move_to(162, -8, 39, 0)
+    Time.sleep(500)
+    Display.print("Ensaio finalizado!")
 
 def main():
     Dobot.setup()
+    showPosition()
     upArm()
-    Dobot.set_home()
-    Time.sleep(2000)
-    iniFirstTray()
-    scanFirstTray()
-    upArm()
-    Dobot.set_home()
-    showPosition
-    
+    iniSecondTray()
+    shakeSecondTray()
+    # upArm()
+    # Dobot.set_home()
+    # Time.sleep(2000)
+    # iniFirstTray()
+    # scanFirstTray()
+    # upArm()
+    # Dobot.set_home()
+    # showPosition()
+    thirdTray()
+    shakeAfterTurnOff()
 
