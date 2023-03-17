c536159ee6dc80196eb74ee8e2adb6d4d58b638b
ViniciosLugli <vinicioslugli@gmail.com>
2023-03-17 15:32:01 -0300

    feat(embedded): :construction: WIP update modules of magic box

--- src/embedded/src/modules/__init__.py
+++ src/embedded/src/modules/__init__.py
@@ -2,3 +2,5 @@ from modules.display import Display
 from modules.time import Time
 from modules.magicbox import MagicBox, PinMode
 from modules.dobot import Dobot
+from modules.keyboard import Keyboard
+from modules.buzzer import Buzzer, Notes
--- src/embedded/src/modules/time.py
+++ src/embedded/src/modules/time.py
@@ -1,7 +1,12 @@
 import dType
+import time
 
 
 class Time:
     @staticmethod
     def sleep(ms: int) -> None:
         dType.dSleep(ms)
+
+    @staticmethod
+    def get_millis() -> int:
+        return time.time()
